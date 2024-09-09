cdf268cf035b0bcd19f39ecd4f3788cb757246b8
zfda1978 <126743052+zfda1978@users.noreply.github.com>
2024-09-09 19:18:11 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import pyodbc 
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+SERVER = '<SQLEXPRESS>'
+DATABASE_NAME = '<Music_01>'
+USERNAME = '<TOSHIBA>'
+PASSWORD = '<Tereliye>'
+
+connectionString = f'DRIVER= {{SQL Server}}; SERVER = SERVER; DATABASE = DATABASE_NAME; USERNAME = USERNAME; PASSWORD = PASSWORD;'
+
+conn = pyodbc.connect(connectionString)
+
+SQL_QUERY="""SELECT * FROM ALBUM"""
+
+cursor = conn.cursor()
+cursor.execute(SQL_QUERY)
+
+records = cursor.fetchall()
+for r in records:
+    print(r)
